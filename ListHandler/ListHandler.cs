d16dac8d112ba01c8e98326e60f3a789a28966ab
orogab25 <orogab25@gmail.com>
2020-03-24 13:31:25 +0100

    Add project files.

---
+++ ListHandler/ListHandler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Basic
+{
+    class ListHandler<T>
+    {
+        //Properties
+        private List<T> list=new List<T>();
+        private Color color;
+        private DateTime created;
+        
+        //Color Structure
+        struct Color
+        {
+            int red;
+            int green;
+            int blue;
+
+            public Color(int red, int green, int blue)
+            {
+                this.red = red;
+                this.green = green;
+                this.blue = blue;
+            }
+        }
+
+        //Constructor
+        public ListHandler(int size,int r,int g,int b)
+        {
+            color = new Color(r,g,b);
+            generateList(size);
+        }
+
+        //Generate list
+        public void generateList(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                list.Add((T)Convert.ChangeType(i, typeof(T)));
+            }
+            created =DateTime.Now;
+        }
+
+        //Save list to file
+        public void writeToFile(string path)
+        {
+            using (TextWriter writer = new StreamWriter(path))
+            {
+                foreach(T row in list)
+                {
+                    writer.WriteLine(row);
+                }
+            }
+        }
+
+        //Load list from file
+        public void readFromFile(string path)
+        {
+            try
+            {
+                using (TextReader reader = new StreamReader(path))
+                {
+                    list.Clear();
+                    string line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        list.Add((T)Convert.ChangeType(line, typeof(T)));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        //Print the list in color
+        public void printList()
+        {
+            Console.WriteLine("List type: " + typeof(T).Name);
+            Console.WriteLine("List created: " + created);
+            Console.WriteLine("List elements:");
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            foreach(T row in list)
+            {
+                Console.WriteLine(row);
+            }
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ListHandler/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu();
+        }
+
+        static void menu()
+        {
+            ListHandler<string> listHandler = new ListHandler<string>(10, 25, 25, 25);
+
+            Console.WriteLine("Please enter the number of the task:");
+            Console.WriteLine("1.Write to file\n2.Read from file\n3.Print list");
+            Console.Write("Choice: ");
+            int choice;
+            bool result = Int32.TryParse(Console.ReadLine(),out choice);
+
+            if (result)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        listHandler.writeToFile("list.txt");
+                        Console.WriteLine("Writing was successful!");
+                        break;
+                    case 2:
+                        listHandler.readFromFile("list.txt");
+                        Console.WriteLine("Reading was successful!");
+                        break;
+                    case 3:
+                        listHandler.printList();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong number!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("The character you entered is not a number!");
+            }
+        }
+    }
+}
